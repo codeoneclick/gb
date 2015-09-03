cbac0c452600fa7979435b885b36bef4577f00cc
codeoneclick <sergey.sergeev.oneclick@gmail.com>
2015-09-03 16:39:31 +0300

    removed unused code

--- gbCore/sources/global_light.cpp
+++ gbCore/sources/global_light.cpp
@@ -11,22 +11,18 @@
 namespace gb
 {
     global_light::global_light(f32 fov, f32 near, f32 far) :
-    m_position(glm::vec3(0.0)),
-    m_look_at(glm::vec3(0.0)),
-    m_distance_to_sun(0.0),
-    m_distance_to_look_at(0.0),
-    m_angle(0.0),
-    m_rotation_center(glm::vec3(0.0))
+    m_position(glm::vec3(0.f)),
+    m_look_at(glm::vec3(0.f))
     {
-        m_matrix_p = glm::perspective(fov, 1.0f, near, far);
+        m_matrix_p = glm::perspective(fov, 1.f, near, far);
     }
     
-    global_light::~global_light(void)
+    global_light::~global_light()
     {
         
     }
     
-    glm::vec3 global_light::get_position(void) const
+    glm::vec3 global_light::get_position() const
     {
         return m_position;
     }
@@ -41,61 +37,23 @@ namespace gb
         m_look_at = look_at;
     }
     
-    glm::vec3 global_light::get_look_at(void) const
+    glm::vec3 global_light::get_look_at() const
     {
         return m_look_at;
     }
     
-    void global_light::set_angle(f32 angle)
-    {
-        m_angle = angle;
-    }
-    
-    f32 global_light::get_angle(void) const
-    {
-        return m_angle;
-    }
-    
-    glm::mat4x4 global_light::get_matrix_v(void) const
+    glm::mat4x4 global_light::get_matrix_v() const
     {
         return m_matrix_v;
     }
     
-    glm::mat4x4 global_light::get_matrix_p(void) const
+    glm::mat4x4 global_light::get_matrix_p() const
     {
         return m_matrix_p;
     }
     
-    void global_light::set_distance_to_sun(f32 distance)
-    {
-        m_distance_to_sun = distance;
-    }
-    
-    void global_light::set_distance_to_look_at(f32 distance)
-    {
-        m_distance_to_look_at = distance;
-    }
-    
-    void global_light::set_rotation_center(const glm::vec3& center)
-    {
-        m_rotation_center = center;
-    }
-    
-    glm::vec3 global_light::get_rotation_center(void) const
-    {
-        return m_rotation_center;
-    }
-    
     void global_light::update(f32 deltatime)
     {
-        /*m_position = glm::vec3(sinf(m_angle) * -m_distance_to_sun + m_rotation_center.x,
-                               cosf(m_angle) * -m_distance_to_sun + m_rotation_center.y,
-                               m_rotation_center.z);
-        
-        glm::vec3 position(sinf(m_angle) * -m_distance_to_look_at + m_look_at.x,
-                           cosf(m_angle) * -m_distance_to_look_at + m_look_at.y,
-                           m_look_at.z);*/
-        
         m_matrix_v = glm::lookAt(m_position, m_look_at, glm::vec3(0.f, 1.f, 0.f));
     }
 }
--- gbCore/sources/global_light.h
+++ gbCore/sources/global_light.h
@@ -21,34 +21,20 @@ namespace gb
         
         glm::mat4 m_matrix_v;
         glm::mat4 m_matrix_p;
-        
-        f32 m_distance_to_sun;
-        f32 m_distance_to_look_at;
-        
-        f32 m_angle;
+
         glm::vec3 m_position;
         glm::vec3 m_look_at;
-        glm::vec3 m_rotation_center;
         
     public:
         
         global_light(f32 fov, f32 near, f32 far);
-        ~global_light(void);
+        ~global_light();
         
-        glm::vec3 get_position(void) const;
+        glm::vec3 get_position() const;
         void set_position(const glm::vec3& position);
         
         void set_look_at(const glm::vec3& look_at);
         glm::vec3 get_look_at(void) const;
-        
-        void set_rotation_center(const glm::vec3& center);
-        glm::vec3 get_rotation_center(void) const;
-        
-        void set_angle(f32 angle);
-        f32 get_angle(void) const;
-        
-        void set_distance_to_sun(f32 distance);
-        void set_distance_to_look_at(f32 distance);
         
         glm::mat4 get_matrix_v(void) const;
         glm::mat4 get_matrix_p(void) const;
